6ae47619ac80c81298054c20a7233cb31a66326b
rhiaro <amy@rhiaro.co.uk>
2022-12-27 21:31:32 +0000

    Rework copy; add menu

--- index.php
+++ index.php
@@ -12,13 +12,13 @@ $delivery = get_delivery_string();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Social share meta tags -->
   <meta property="og:title" content="The Tiffin">
-  <meta property="og:description" content="Homecooked Punjabi street food, delivered to you, in Kirkcaldy and Fife">
+  <meta property="og:description" content="Homecooked Punjabi Indian street food, delivered to you, in Kirkcaldy and Fife">
   <meta property="og:type" content="website">
   <meta property="og:image" content="img/foodpic.jpg">
   <meta property="og:site_name" content="The Tiffin">
   <meta name="twitter:card" content="summary_large_image">
   <meta name="twitter:title" content="The Tiffin">
-  <meta name="twitter:description" content="Homecooked Punjabi street food, delivered to you, in Kirkcaldy and Fife">
+  <meta name="twitter:description" content="Homecooked Punjabi Indian street food, delivered to you, in Kirkcaldy and Fife">
   <meta name="twitter:image:src" content="img/foodpic.jpg">
   <!-- End social share meta tags -->
   <link rel="shortcut icon" href="img/favicon.png">
@@ -26,7 +26,7 @@ $delivery = get_delivery_string();
   <link rel="stylesheet" href="css/tiffin.css">
   <link rel="stylesheet" type="text/css" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" />
   <title>The Tiffin</title>
-  <meta name="description" content="Homecooked Punjabi street food, delivered to you, in Kirkcaldy and Fife">
+  <meta name="description" content="Homecooked Punjabi Indian street food, delivered to you, in Kirkcaldy and Fife">
 </head>
 <body>
   <header>
@@ -34,45 +34,43 @@ $delivery = get_delivery_string();
   </header>
   <main>
     <section id="intro">
-      <h2>Delicious vegan Punjabi street food, from our home to yours.</h2>
+      <h2>Delicious Punjabi street food, from our home to yours.</h2>
       <p>
-Each week we offer a set menu for delivery, including starters, mains, dahl, rice and chapati.
-Simply tell us how many people you want to order for, and a 30 minute delivery window,
-and we will bring your food hot and fresh and ready to eat.
-All you need to do is set the table.
-      </p>
-      <p class="center-sm">
-<a href="#menu" class="btn">Order this week's menu</a>
-<span class="social">
-  <a href="https://www.facebook.com/TheTiffin2020" target="_blank"><img src="img/icon_fb.png" alt="Facebook" title="The Tiffin on Facebook" /></a>
-  <a href="https://wa.me/447480112136" target="_blank"><img src="img/icon_wa.png" alt="WhatsApp" title="The Tiffin on Whatsapp" /></a>
-  <a href="#" target="_blank"><img src="img/icon_insta.png" alt="Instagram" title="The Tiffin on Instagram" /></a>
-  <a href="tel:+447480112136"><img src="img/icon_phone.png" alt="Telephone" title="Contact The Tiffin by phone" /></a>
-</span>
+Let us cook for you in our <a href="#contact">newly refurbished restaurant</a> at 89 Victoria Road, Kirkcaldy,
+or <a href="#order">order now</a> for delivery to enjoy at home.
+
+We offer delicious homemade samosa and pakora, a selection of rich and spicy curry dishes,
+a variety of creamy dahls, soft chapatis, and fragrant rice.
+We cook everything ourselves from scratch, using traditional Indian methods, and local Scottish ingredients.
+Our customers say we serve the best Indian takeaway in Kirkcaldy!
       </p>
       <p>
-<?if($delivery):?>
-  <?=$delivery?>
-<?else:?>
-  We deliver on <strong>Fridays and Saturdays</strong>, from <strong>5:30pm to 9:30pm</strong>.
-<?endif?>
-      </p>
-      <p>
-Gluten-free meals are available - please let us know when ordering.
+All of our dishes are vegetarian and vegan friendly, and can be made gluten-free - just let us know when ordering.
       </p>
     </section>
-    <section id="menu">
-      <img src="<?=$latest_menu?>" />
-      <p><em>Menu last updated: <?=$menu_date?></em></p>
+    <section id="hours">
+<!--       <p>
+        <img src="img/gallery/beans.jpg" alt="A glass bowl filled with a rich spicy black-eyed bean dahl, garnished with coriander." />
+      </p> -->
+      <h3>Opening times</h3>
+      <ul>
+<li><p>Tuesday to Thursday: 5pm - 10pm</p></li>
+<li><p>Friday to Saturday: 12pm - 10pm</p></li>
+<li><p>Sunday to Monday: closed</p></li>
+      </ul>
     </section>
     <section id="order">
-      <h3>Place your order by social media or phone</h3>
-      <p class="social">
-        <a href="https://www.facebook.com/TheTiffin2020" target="_blank"><img src="img/icon_fb.png" alt="Facebook" title="The Tiffin on Facebook" /></a>
-        <a href="https://wa.me/447480112136" target="_blank"><img src="img/icon_wa.png" alt="WhatsApp" title="The Tiffin on Whatsapp" /></a>
-        <a href="#" target="_blank"><img src="img/icon_insta.png" alt="Instagram" title="The Tiffin on Instagram" /></a>
-        <a href="tel:+447480112136"><img src="img/icon_phone.png" alt="Telephone" title="Contact The Tiffin by phone" /></a>
+      <h3>Order for delivery by social media or phone</h3>
+      <p class="center-sm">
+<a href="#menu" class="btn">See our menu</a>
+        <span class="social">
+          <a href="https://www.facebook.com/TheTiffin2020" target="_blank"><img src="img/icon_fb.png" alt="Facebook" title="The Tiffin on Facebook" /></a>
+          <a href="https://wa.me/447480112136" target="_blank"><img src="img/icon_wa.png" alt="WhatsApp" title="The Tiffin on Whatsapp" /></a>
+          <a href="#" target="_blank"><img src="img/icon_insta.png" alt="Instagram" title="The Tiffin on Instagram" /></a>
+          <a href="tel:+447480112136"><img src="img/icon_phone.png" alt="Telephone" title="Contact The Tiffin by phone" /></a>
+        </span>
       </p>
+    </section>
     </section>
   </main>
   <aside>
@@ -99,6 +97,63 @@ Priya and Tony run the business, helped by our little princess who plays a vital
 Priya originally comes from Delhi in India, and has been taught everything from her mother and grandmother, using traditional methods that have been running in the family for many generations.
 We started the business as a trial upon requests from friends and family who loved Priya's cooking and always said we must do this professionally.
       </p>
+    </section>
+    <section id="menu">
+      <h3>Starters</h3>
+      <div id="dishes">
+        <div>
+<h4>Vegetable samosa</h4>
+<span class="price">&pound;3.95</span>
+<p>Crispy pastry pockets stuffed with delicious spiced vegetables and potato</p>
+        </div>
+        <div>
+<h4>Haggis samosa</h4>
+<span class="price">&pound;3.95</span>
+<p>Traditional samosa with a Scottish twist</p>
+        </div>
+        <div>
+<h4>Onion bhaji</h4>
+<span class="price">&pound;2.95</span>
+<p>Crispy battered onion, served with chutney</p>
+        </div>
+        <div>
+<h4>Avocado pakora</h4>
+<span class="price">&pound;2.95</span>
+<p>Soft avocado in crispy batter, served with chutney</p>
+        </div>
+      </div>
+      <h3>Main dishes</h3>
+      <div id="dishes">
+        <div>
+<h4>Khadi pakora</h4>
+<span class="price">&pound;9.95</span>
+<p>Everyone's favourite gram flour curry</p>
+        </div>
+        <div>
+<h4>Bombay potatoes</h4>
+<span class="price">&pound;9.95</span>
+<p>Potato wedges slow cooked in rich masala</p>
+        </div>
+        <div>
+<h4>Aloo gobi</h4>
+<span class="price">&pound;2.95</span>
+<p>Cauliflower and potato curry</p>
+        </div>
+      </div>
+      <h3>Side dishes</h3>
+      <div id="dishes">
+        <div>
+<h4>Jeera rice</h4>
+<span class="price">&pound;1.95</span>
+<p>Rice with cumin seeds</p>
+        </div>
+        <div>
+<h4>Etc etc</h4>
+<span class="price">&pound;2.95</span>
+<p>And so on...</p>
+        </div>
+
+      </div>
     </section>
   </aside>
   <footer>
